b2a8000f360206784c15ddb898653f9ef1f73b30
nikit <nikita_eremeev.95@mail.ru>
2024-02-06 14:02:09 +0300

    registration screen

---
+++ app/src/main/java/com/tz/online_shop/ui/login/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.tz.online_shop.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tz.online_shop.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
